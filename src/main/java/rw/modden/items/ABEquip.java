9f45edef0c55b690da7bb329b56b9e54b42144e4
JimForse <flamefors8@gmail.com>
2026-07-30 00:26:35 +0300

    Thirtieth

--- src/main/java/rw/modden/items/ABEquip.java
+++ src/main/java/rw/modden/items/ABEquip.java
@@ -1,4 +1,30 @@
 package rw.modden.items;
 
+import net.minecraft.nbt.NbtCompound;
+import net.minecraft.server.network.ServerPlayerEntity;
+
 public class ABEquip {
+    private String id, itemID;
+    private double healReserveBonus, healRegenBonus;
+
+    public ABEquip (String uniqueID, String itemID, double healReserveBonus, double healRegenBonus) {
+        this.id = uniqueID;
+        this.itemID = itemID;
+        this.healRegenBonus = healRegenBonus;
+        this.healReserveBonus = healReserveBonus;
+    }
+
+    public void readFromNbt(NbtCompound nbt) {
+        this.itemID = nbt.getString("unique_id"); // first line
+        this.healReserveBonus = nbt.getDouble("healReserveBonus"+itemID);
+        this.healRegenBonus = nbt.getDouble("healRegenBonus"+itemID);
+        this.id = nbt.getString("item_id"+itemID);
+    }
+
+    public void writeToNbt(NbtCompound nbt) {
+        nbt.putString("item_id: "+itemID, id);
+        nbt.putDouble("healReserveBonus"+itemID, healReserveBonus);
+        nbt.putDouble("healRegenBonus"+itemID, healRegenBonus);
+        nbt.putString("unique_id", itemID); // last line
+    }
 }
